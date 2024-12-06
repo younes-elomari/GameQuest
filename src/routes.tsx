ab193163bf7490e2522a6082cae78f71463a350f
Younes Elaomari <youneselaomari@gmail.com>
2024-12-06 11:04:39 +0000

    Handle errors

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import GameDetailsPage from "./pages/GameDetailsPage";
 import Layout from "./pages/layout";
+import ErrorPage from "./pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { path: "", element: <HomePage /> },
       { path: "games/:id", element: <GameDetailsPage /> },
